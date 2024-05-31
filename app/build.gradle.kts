07e79b929ca782c15e2a7bf53fc4d3d9aa628503
sheldondcosta <ssheldondcosta@gmail.com>
2024-05-31 15:22:16 -0400

    navigation dependency added and login screen added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,4 +85,9 @@ dependencies {
     // Hilt
     implementation("com.google.dagger:hilt-android:2.51.1")
     kapt("com.google.dagger:hilt-compiler:2.51.1")
+
+    // Navigation
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.5.0")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.5.0")
+    implementation ("androidx.navigation:navigation-compose:2.5.0")
 }
---
+++ app/src/main/java/com/example/spacexcleanmvi/ui/compose/nav/login/LoginScreen.kt
@@ -0,0 +1,8 @@
+package com.example.spacexcleanmvi.ui.compose.nav.login
+
+import androidx.navigation.NavController
+
+fun LoginScreen(navController: NavController) {
+
+
+}
