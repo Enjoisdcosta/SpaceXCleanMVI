d776ae0c88729a10af3de487861eb236f2317684
sheldondcosta <ssheldondcosta@gmail.com>
2024-05-31 14:24:38 -0400

    Clean architecture packages added

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -41,4 +41,16 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    //okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    //gson
+    implementation("com.google.code.gson:gson:2.10.1")
 }
