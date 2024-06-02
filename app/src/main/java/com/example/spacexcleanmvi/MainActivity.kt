dad6d533c08222946901738a55a74adf6e18af5f
sheldondcosta <ssheldondcosta@gmail.com>
2024-06-02 13:58:03 -0400

    Domain screen class added and dependency in gradle

--- app/src/main/java/com/example/spacexcleanmvi/MainActivity.kt
+++ app/src/main/java/com/example/spacexcleanmvi/MainActivity.kt
@@ -5,23 +5,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.common.nav.NavRoutes
 import com.example.common.nav.NavRoutes.Companion.ROUTE_LOGIN
 import com.example.common.nav.NavRoutes.Companion.ROUTE_SIGN_UP
 import com.example.spacexcleanmvi.ui.compose.nav.login.ui.CleanArchitectureTheme
@@ -41,6 +59,7 @@ class MainActivity : ComponentActivity() {
             CleanArchitectureTheme {
                 val navController = rememberNavController()
                 Nav(navController)
+
 
 
 
@@ -73,16 +92,15 @@ fun Nav(navController: NavController) {
 
 }
 @Composable
+fun App(navController: NavHostController) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val bottomBarState = remember { mutableStateOf(true) }
+
+}
+
+
+@Composable
 fun BottomAppBar(navController: NavHostController) {
-    var showSignOutDialog by remember { mutableStateOf(false) }
-
-    BottomNavigation(
-        backgroundColor = Color.White,
-        contentColor = Color.Black
-    ){
-
-    }
-
 }
 
 
