daa3d893f156cc43793fc5b3665dc3cdfd7e043b
sheldondcosta <ssheldondcosta@gmail.com>
2024-06-02 23:04:39 -0400

    More DI

--- data/src/main/java/com/example/data/remote/di/MainModule.kt
+++ data/src/main/java/com/example/data/remote/di/MainModule.kt
@@ -3,6 +3,10 @@ package com.example.data.remote.di
 
 import com.example.data.remote.api.APIDetail
 import com.example.data.remote.api.APIEndpoints
+import com.example.data.remote.api.RetrofitInstance
+import com.example.data.remote.domain.FetchDataUseCase
+import com.example.data.remote.repository.CapsuleRepository
+import com.example.data.remote.repository.CapsuleRepositoryImplementaion
 import com.example.virginmoney.data.repository.Repository
 import com.example.virginmoney.data.repository.RepositoryImplementaion
 import com.google.gson.Gson
@@ -56,5 +60,21 @@ object MainModule {
     @Provides
     fun providesRepository(apiEndpoints: APIEndpoints): Repository {
         return RepositoryImplementaion(apiEndpoints)
+    }
+
+
+    @Provides
+    fun provideApiService(): APIEndpoints {
+        return RetrofitInstance.api
+    }
+
+    @Provides
+    fun provideDataRepository(apiService: APIEndpoints): CapsuleRepository {
+        return CapsuleRepositoryImplementaion(apiService)
+    }
+
+    @Provides
+    fun provideFetchDataUseCase(repository: CapsuleRepository): FetchDataUseCase {
+        return FetchDataUseCase(repository)
     }
 }
