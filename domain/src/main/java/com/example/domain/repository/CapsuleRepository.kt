abe21421aaefc5def3673206bef651095ed85296
sheldondcosta <ssheldondcosta@gmail.com>
2024-06-03 20:02:49 -0400

    Getting data from the api and its printing

--- domain/src/main/java/com/example/domain/repository/CapsuleRepository.kt
+++ domain/src/main/java/com/example/domain/repository/CapsuleRepository.kt
@@ -1,6 +1,7 @@
 package com.example.domain.repository
 
 import com.example.domain.entity.Capsule
+import com.example.domain.entity.History
 import kotlinx.coroutines.flow.Flow
 
 interface CapsuleRepository {
@@ -8,4 +9,6 @@ interface CapsuleRepository {
     fun getCapsules(): Flow<List<Capsule?>?>
 
     fun getCapsuleById(capsuleId: String?): Flow<Capsule>
+
+
 }
--- domain/src/main/java/com/example/domain/repository/HistoryRepository.kt
+++ domain/src/main/java/com/example/domain/repository/HistoryRepository.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface HistoryRepository {
 
+
     fun getHistory(): Flow<List<History?>?>
     fun getHistoryItem(id: Int?): Flow<History>
 }
